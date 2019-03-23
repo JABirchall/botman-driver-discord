645afef8df9c0bbf482cb39336145567bf072f24
JABirchall <JABirchall@users.noreply.github.com>
2019-03-23 15:04:08 +0000

    Commit config stub

--- stubs/discord.php
+++ stubs/discord.php
@@ -9,4 +9,15 @@ return [
     |
     */
     'token' => env('SLACK_TOKEN'),
+    'options' => [
+        'disableClones' => true,
+        'disableEveryone' => true,
+        'fetchAllMembers' => false,
+        'messageCache' => true,
+        'messageCacheLifetime' => 600,
+        'messageSweepInterval' => 600,
+        'presenceCache' => false,
+        'userSweepInterval' => 600,
+        'ws.disabledEvents' => ['TYPING_START'], // Do not remove TYPING_START
+    ],
 ];
